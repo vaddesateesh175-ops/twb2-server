698ab38fd56c25f41ab2de88b069d874ec7f2459
sateesh <vaddesateesh175@gmail.com>
2026-05-18 17:11:07 +0530

    library

---
+++ router/libraryrouter.js
@@ -0,0 +1,64 @@
+const express=require("express");
+var router=express.Router();
+var book=require("../model/library.model");
+
+
+router.post("/addbook", (req,res)=>{   
+    console.log(req.body);
+    const {username,booktitle,author}=req.body;
+    book.findOne({booktitle})
+    .then((already)=>{
+        if(already){
+           return res.send({msg:"Book is not available"});
+        }
+        var newbook=new book({
+           username,booktitle,author
+        });
+       newbook.save()
+       .then((data)=>{
+          console.log(data);
+          return res.send({msg:"book added successfully",data});
+        })
+        .catch((err)=>{
+           console.log(err);
+           return res.send({msg:"error in adding book"});
+      })
+
+    })
+    .catch((err)=>{
+        console.log(err);
+        return res.send({
+            msg: "error in checking book"
+        });
+    })
+})
+
+
+router.delete("/:id",(req,res)=>{
+    book.findByIdAndDelete(_id=req.params.id).then((data)=>{
+        res.send({msg:"book deleted successfully"})
+    })
+})
+
+router.get("/:id",(req,res)=>{
+    book.findById(_id=req.params.id).then((data)=>{
+        res.send(data)
+    })
+})
+
+
+    
+router.get("/books/:username",(req,res)=>{
+    book.find({username:req.params.username})
+    .then((data)=>{
+        res.send(data)
+    })
+    .catch((err)=>{
+        console.log(err);
+        res.send({msg:"error in getting books"});
+    }) 
+})
+
+
+
+module.exports=router;
--- server.js
+++ server.js
@@ -8,81 +8,40 @@ var jwt = require("jsonwebtoken");
 var cors = require("cors");
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
-const multer  = require('multer')
 
-app.use(express.static(__dirname + "/uploads"));
+
+var login=require("./router/loginrouter");
+var books=require("./router/booksrouter");
+var mybooks=require("./router/libraryrouter");
+
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const PhotoModel=require("./model/photo.model")
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './uploads')
-  },
-  filename: function (req, file, cb) {
-    console.log(req.body)
-    console.log("file details",file)
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, uniqueSuffix+ '-' +file.originalname  )
-  }
-})
-const upload = multer({ storage})
 
 var connectDB = require("./db");
 const dns = require("dns");
 dns.setServers(["1.1.1.1","8.8.8.8"])
 
 connectDB();
-
 app.use(cors());
 
-app.post("/uploadPhoto", upload.single("photo"), (req, res) =>{
-  res.send({msg:"agurababu"})
-  
-  var k=jwt.verify(req.headers.token,"neekendu");
-  console.log(k);
-  // console.log(req.headers.token);
-  var newPhoto=new photomodel({
-    photoUrl:req.file.filename,
-    username:k.username,
-  })
-  newPhoto.save()
+
+app.use("/",login);
+app.use("/",books);
+app.use("/",mybooks)
+app.get("/books",async (res,req)=>{
+  const data = await book.find();
+  res.send(data);
 })
 
-app.get("/photos",(req,res)=>{
-  var {username}=jwt.verify(req.headers.token,"neekendu");
-   PhotoModel.find({ username: username }).then((data) => {
-    res.send(data);
-  });
+app.get("/books/:username",(req,res)=>{
+  const data = book.find({username:req.params.username});
+  res.send(data);
 })
 
-app.get("allphotos",(req,res)=>{
-  PhotoModel.find().then((data)=>{
-    res.send(data);
-  })
-})
-  
 
-app.post("/login", (req, res) => {
-  console.log(req.body);
-  var fd = JSON.parse(fs.readFileSync(__dirname + "/users.txt").toString());
-  var k = fd.find((user) => {
-    if (
-      user.username === req.body.username &&
-      user.password === req.body.password
-    ) {
-      return true;
-    }
-  });
-  if (k) {
-    //gen token
-    var token = jwt.sign({ ...req.body }, "neekendu");
-    res.send({ msg: "loginsuccess", token, username: req.body.username });
-  } else {
-    res.send({ msg: "loginfailed" });
-  }
-});
 
 app.listen(process.env.PORT || 3600, () => {
   console.log(process.env.PORT || 3600, "port lo server start aindi");
